eb82a93c75512f3539d43e44f8f82e138b047b5a
HappySingh7837 <46443936+HappySingh7837@users.noreply.github.com>
2020-09-28 09:32:00 +0530

    before starting api service

---
+++ lib/freezed_data_model/domain/entities/git_entity_freezed.dart
@@ -0,0 +1,12 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// part 'git_entity_frezzed.freezed.dart';
+
+// @freezed
+// abstract class ProjectEntity  implements _$ProjectEntity{
+//     const ProjectEntity._();
+//   factory ProjectEntity({
+//     final String userName,
+//     final String projectName,
+//   })=_ProjectEntity;
+
+// }
